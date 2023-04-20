f379b5525f296250fff12c2459bacab05dba0174
Evelyn Helena <evelynhelena781@gmail.com>
2023-04-19 22:18:07 -0300

    Add files via upload

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,28 @@
+import { Button, TextField } from "@mui/material"
+import "./style.css";
+import { useState } from "react";
+
+function App() {
+  const [textTarefa,setTextTarefa] = useState("");
+
+  function handleClick(){
+    console.log(textTarefa);
+  }
+
+  return (
+    <form className="form-container">
+      <TextField 
+        value={textTarefa}
+        id="standard-basic" 
+        label="Tarefa" 
+        variant="standard" 
+        placeholder="Digite a tarefa"
+        onChange={({target}) => setTextTarefa(target.value)}
+      />
+      <Button variant="contained" onClick={handleClick}>Add</Button>
+
+    </form>
+  )
+}
+
+export default App
