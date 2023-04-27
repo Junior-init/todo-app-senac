0cc8f8cb278aead3924f42bc2c92aa1c64d213ee
Evelyn Helena <evelynhelena781@gmail.com>
2023-04-26 22:12:43 -0300

    atualizando

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -2,26 +2,55 @@ import { Button, TextField } from "@mui/material"
 import "./style.css";
 import { useState } from "react";
 
-function App() {
-  const [textTarefa,setTextTarefa] = useState("");
 
-  function handleClick(){
-    console.log(textTarefa);
+
+function App() {
+  const [textTarefa, setTextTarefa] = useState("");
+  const [listTarefa, setListTarefa] = useState([]);
+
+  function handleClick() {
+    setListTarefa((old) => [...old, textTarefa]);
+    setTextTarefa("");
+  }
+
+  function handleDelete(){
+    setListTarefa([])
   }
 
   return (
-    <form className="form-container">
-      <TextField 
-        value={textTarefa}
-        id="standard-basic" 
-        label="Tarefa" 
-        variant="standard" 
-        placeholder="Digite a tarefa"
-        onChange={({target}) => setTextTarefa(target.value)}
-      />
-      <Button variant="contained" onClick={handleClick}>Add</Button>
+    <>
+      <form className="form-container">
+        <TextField
+          value={textTarefa}
+          id="standard-basic"
+          label="Tarefa"
+          variant="standard"
+          placeholder="Digite a tarefa"
+          onChange={({ target }) => setTextTarefa(target.value)}
+        />
+        <Button variant="contained" onClick={handleClick}>
+          Add
+        </Button>
+        <Button variant="contained" color="secondary" 
+        onClick={handleDelete}>
+          Deletar
+        </Button>
 
-    </form>
+      </form>
+
+      {
+        listTarefa.map((tarefa, index) => (
+          <>
+            <div key={String(index)}>
+              {tarefa}
+            </div>
+
+            <div className="divider"/>
+          </>
+        ))
+      }
+
+    </>
   )
 }
 
